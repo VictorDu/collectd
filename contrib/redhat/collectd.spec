22c780b375ef3925384c4233381466fc39065dff
VictorDu <wentao@signalfuse.com>
2015-06-29 22:47:16 +0000

    change version

--- contrib/redhat/collectd.spec
+++ contrib/redhat/collectd.spec
@@ -217,7 +217,7 @@
 
 Summary:	statistics collection and monitoring daemon
 Name:		collectd
-Version:	SingalFx.5.5.0
+Version:	5.5.0.sfx
 Release:	1%{?dist}
 URL:		http://collectd.org
 Source:		http://collectd.org/files/%{name}-%{version}.tar.bz2
--- version-gen.sh
+++ version-gen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DEFAULT_VERSION="5.5.0.git"
+DEFAULT_VERSION="5.5.0.sfx"
 
 VERSION="`git describe 2> /dev/null | grep collectd | sed -e 's/^collectd-//'`"
 
@@ -8,6 +8,7 @@ if test -z "$VERSION"; then
 	VERSION="$DEFAULT_VERSION"
 fi
 
+	VERSION="$DEFAULT_VERSION"
 VERSION="`echo \"$VERSION\" | sed -e 's/-/./g'`"
 
 printf "%s" "$VERSION"
